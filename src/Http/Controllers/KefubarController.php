4765b399b53b7555a2c66f23962b759b46e9aa92
yangG <3664839@qq.com>
2024-10-15 20:43:27 +0800

    up

--- src/Http/Controllers/KefubarController.php
+++ src/Http/Controllers/KefubarController.php
@@ -5,14 +5,16 @@ namespace Dcat\Admin\Kefubar\Http\Controllers;
 use Dcat\Admin\Layout\Content;
 use Dcat\Admin\Admin;
 use Illuminate\Routing\Controller;
-
+use Dcat\Admin\Widgets\Alert;
 class KefubarController extends Controller
 {
     public function index(Content $content)
     {
+        $alert = Alert::make('这是一个提示','提示');
         return $content
             ->title('客服专用条')
             ->description('一键配置')
-            ->body(Admin::view('ycookies.kefubar::index'));
+            ->row($alert->info())
+            ->row(Admin::view('ycookies.kefubar::index'));
     }
 }
